815ee25f8591693dfc36256db310c7a18658959c
Black Wolf <67378732+Abdullah-js@users.noreply.github.com>
2021-01-17 19:00:47 +0300

    Math problem
    ;)

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const readline = require('readline')
+const rl = readline.createInterface({input : process.stdin,
+                         output : process.stdout})
+let num1 = Math.floor((Math.random()* 100) + 1);
+let num2 = Math.floor((Math.random()* 100) + 1);
+let answer = num1 + num2;
+
+rl.question(`NE LAN BU ${ num1 } + ${ num2 }?`,
+(userInput)=>{
+    if(userInput.trim() == answer){
+        rl.close();
+    }
+});
