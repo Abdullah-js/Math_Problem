615872f1950d08fccdcc754ba308ab6cd76ad2e2
Black Wolf <67378732+Abdullah-js@users.noreply.github.com>
2021-01-18 12:19:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-
 const readline = require('readline')
 const rl = readline.createInterface({input : process.stdin,
                          output : process.stdout})
-let num1 = Math.floor((Math.random()* 100) + 1);
-let num2 = Math.floor((Math.random()* 100) + 1);
+let num1 = Math.floor((Math.random()* 10) + 1);
+let num2 = Math.floor((Math.random()* 10) + 1);
 let answer = num1 + num2;
 
-rl.question(`NE LAN BU ${ num1 } + ${ num2 }?`,
+rl.question(`NE LAN BU ${ num1 } + ${ num2 } ?`,
 (userInput)=>{
     if(userInput.trim() == answer){
         rl.close();
     }else{
-        console.log('hata yaptın mallll')
-        rl.close();
+       rl.setPrompt('hata yaptın mallll');
+       rl.prompt();
+       rl.on('line',(userInput)=>{
+           if(userInput.trim() == answer)
+                rl.close();
+          else{
+              rl.setPrompt(` ${ userInput } <= yanlış lan bu bidaha dene `)
+              rl.prompt();
+          }
+       })
     }
 });
+
+rl.on('close',()=>{
+    console.log('şasırdım lan  doğru!!!!')
+})
